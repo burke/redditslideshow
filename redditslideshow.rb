ce630ae196b62985ff6a1dde812931121fef4f5d
Burke Libbey <burke@burkelibbey.org>
2010-04-03 23:52:16 -0500

    changes.

--- redditslideshow.rb
+++ redditslideshow.rb
@@ -2,8 +2,19 @@ require 'rubygems'
 require 'sinatra'
 require 'json'
 require 'open-uri'
+require 'erb'
+
+def make_slideshow
+  JSON.
+    parse(open("http://reddit.com/r/pics/top.json").read).
+    fetch('data').
+    fetch('children').
+    map{|e|e['data']}.
+    select{|e|%{gif jpg png}.include? e['url'][-3..-1]}.
+    map{|x|"<a href='#{x['permalink']}'><img src='#{x['url']}' title='#{x['title']}'/></a>"}.
+    join("\n")
+end
 
 get '/' do
-  links = JSON.parse(open("http://reddit.com/r/pics/top.json").read)['data']['children']
-  links.inspect
+  erb :index, :locals => {:slideshow => make_slideshow}
 end
